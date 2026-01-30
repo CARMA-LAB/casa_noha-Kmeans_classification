bcb739bff4f18e2c962a23e8862ef36046c416b7
CARMA-LAB <studioguida.a408@outlook.it>
2026-01-30 15:29:12 +0100

    Initial commit

---
+++ pointclouds/casanoha_kmean_classification.las_converted/cloud.js
@@ -0,0 +1,43 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 6733833,
+    "boundingBox": {
+        "lx": 22.099929809570314,
+        "ly": -17.379037857055665,
+        "lz": 23.815,
+        "ux": 56.695929809570319,
+        "uy": 17.21696214294434,
+        "uz": 58.411
+    },
+    "tightBoundingBox": {
+        "lx": 22.099929809570314,
+        "ly": -17.379037857055665,
+        "lz": 23.815,
+        "ux": 56.695929809570319,
+        "uy": 13.856962142944337,
+        "uz": 50.496
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.2996101379394531,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
